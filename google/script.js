2c34390a9ec561eb885b6cff591af4e92a9c72c0
ViniciusMvWick <129290291+ViniciusMvWick@users.noreply.github.com>
2023-11-25 19:12:26 -0300

    Delete google directory

--- google/script.js
+++
@@ -1,8 +0,0 @@
-const search = document.querySelector('.search')
-const btn = document.querySelector('.btn')
-
-btn.addEventListener('click', () => {
-    const url = 'https://www.google.com.br/search?q=' + search.value;
-    window.open(url, '_self');
-})
-
